a010e81a2e4d0087b6c6028f28d9daaf7362e77e
MicA <mihaela.botnaru98@gmail.com>
2020-07-02 12:44:49 +0300

    Initial commit

---
+++ src/main/java/com/kids_app/kids_app/HomeController.java
@@ -0,0 +1,12 @@
+package com.kids_app.kids_app;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Hello, World";
+    }
+}
